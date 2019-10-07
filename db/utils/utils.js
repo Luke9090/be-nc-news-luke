212d81805e87ee4d3b27dd5be5bc32f588ed8f32
Luke Horne <luke.horne@live.co.uk>
2019-10-07 12:01:45 +0100

    Utils - formatDates - passing first three tests

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,9 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const newList = list.map(obj => {
+    return { ...obj };
+  });
+  return newList;
+};
 
 exports.makeRefObj = list => {};
 
