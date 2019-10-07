aca355ee5b0fc6e9732b6b22c9f9e6732e14fdd1
Luke Horne <luke.horne@live.co.uk>
2019-10-07 11:52:31 +0100

    Utils - formatDates tests written

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,11 +1,80 @@
 const { expect } = require('chai');
-const {
-  formatDates,
-  makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+const { formatDates, makeRefObj, formatComments } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('returns a new array without mutating the original array', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly'
+      }
+    ];
+    expect(formatDates(input)).to.be.an('array');
+    expect(formatDates(input)).to.not.equal(input);
+  });
+  it('objects returned in array have all properties other than "created_at" equal to their original value', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly'
+      },
+      {
+        title: "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        author: 'jessjelly'
+      }
+    ];
+    const control = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly'
+      },
+      {
+        title: "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        author: 'jessjelly'
+      }
+    ];
+    expect(formatDates(input)).to.eql(control);
+  });
+  it('first object returned in array has a created_at value which has been converted to a JS date', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        created_at: 1471522072389
+      },
+      {
+        title: "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        created_at: 1500584273256
+      }
+    ];
+    const expected = new Date(1471522072389);
+    expect(formatDates(input)[0].created_at).to.equal(expected);
+  });
+  it('all objects returned in array have a created_at value which has been converted to a JS date', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        created_at: 1471522072389
+      },
+      {
+        title: "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        created_at: 1500584273256
+      }
+    ];
+    const expected1 = new Date(1471522072389);
+    expect(formatDates(input)[0].created_at).to.equal(expected1);
+    const expected2 = new Date(1500584273256);
+    expect(formatDates(input)[1].created_at).to.equal(expected2);
+  });
+});
 
 describe('makeRefObj', () => {});
 
