bc146903b1abaddc5eb6c22feb1e985de617822e
Luke Horne <luke.horne@live.co.uk>
2019-10-08 11:10:55 +0100

    Endpoints - GET api/username/:invalid_username Passes test for handling of invalid usernames

--- app/models/users-model.js
+++ app/models/users-model.js
@@ -4,7 +4,8 @@ exports.selectUserByUsername = username => {
   return knex('users')
     .select('*')
     .where('username', username)
-    .then(([user]) => {
-      return { user };
+    .then(userArr => {
+      if (userArr.length === 0) return Promise.reject({ status: 404, msg: `Could not find a user with the username "${username}"` });
+      else return { user: userArr[0] };
     });
 };
