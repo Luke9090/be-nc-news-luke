6f3dc36db43600cc268edd68a8a11410d628f147
Luke Horne <luke.horne@live.co.uk>
2019-10-08 11:38:23 +0100

    GET api/articles/:article_id - tests written Including tests for invalid and non-existent article IDs

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -5,6 +5,8 @@ const { expect } = require('chai');
 const knex = require('../connection');
 const app = require('../app/app');
 
+const testData = require('../db/data');
+
 describe('/', () => {
   beforeEach(() => knex.seed.run());
   after(() => knex.destroy());
@@ -61,13 +63,53 @@ describe('/', () => {
             expect(body.user).to.eql(expected);
           });
       });
-      it('GET /:invalid_username - responds 404 with an object containing an error message under the key "err"', () => {
+      it('GET /:non-existent-username - responds 404 with an object containing an error message under the key "err"', () => {
         return request(app)
           .get('/api/users/non-existent-user')
           .expect(404)
           .then(({ body }) => {
             expect(body).to.have.key('err');
             expect(body.err).to.eql('Could not find a user with the username "non-existent-user"');
+          });
+      });
+    });
+    describe('/articles', () => {
+      it('GET /:article_id - responds 200 with an object containing an article object under the key "article"', () => {
+        return request(app)
+          .get('/api/articles/1')
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).to.have.key('article');
+            expect(body.article).to.have.keys('author', 'title', 'article_id', 'body', 'topic', 'created_at', 'votes', 'comment_count');
+            const expected = {
+              author: testData.articleData[0].author,
+              title: testData.articleData[0].title,
+              article_id: 1,
+              body: testData.articleData[0].body,
+              topic: testData.articleData[0].topic,
+              created_at: '2018-11-15 12:21:54.171+00',
+              votes: 100,
+              comment_count: 13
+            };
+            expect(body.article).to.eql(expected);
+          });
+      });
+      it('GET /:invalid_article_id - responds 400 with an object containing an error message under the key "err"', () => {
+        return request(app)
+          .get('/api/articles/invalid-article-id')
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.key('err');
+            expect(body.err).to.eql('"invalid-article-id" is not a valid article ID. Expected a number.');
+          });
+      });
+      it('GET /:non-existent_article_id - responds 404 with an object containing an error message under the key "err"', () => {
+        return request(app)
+          .get('/api/articles/9999')
+          .expect(404)
+          .then(({ body }) => {
+            expect(body).to.have.key('err');
+            expect(body.err).to.eql('Could not find an article with the article ID "9999".');
           });
       });
     });
