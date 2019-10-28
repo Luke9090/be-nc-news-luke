be76f008865868b741df3e95cdd1200fb14df83c
Luke Horne <luke.horne@live.co.uk>
2019-10-28 14:50:56 +0000

    Added CORS middleware

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 const apiRouter = require('./routers/api-router');
 const errHandlers = require('./error-handlers');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
