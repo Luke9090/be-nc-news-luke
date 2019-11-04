640d777e15d08875c85ad95aec40b1a00db23d7a
Luke Horne <luke.horne@live.co.uk>
2019-11-04 09:34:58 +0000

    Added article and comment counts to user endpoints

--- app/models/users-model.js
+++ app/models/users-model.js
@@ -2,7 +2,11 @@ const knex = require('../../connection');
 
 exports.selectUsers = () => {
   return knex('users')
-    .select('*')
+    .select('username', 'avatar_url', 'name')
+    .countDistinct({ comment_count: 'comment_id', article_count: 'title' })
+    .leftJoin('articles', 'articles.author', 'username')
+    .leftJoin('comments', 'comments.author', 'username')
+    .groupBy('username')
     .then(users => {
       return { users };
     });
@@ -10,7 +14,11 @@ exports.selectUsers = () => {
 
 exports.selectUserByUsername = username => {
   return knex('users')
-    .select('*')
+    .select('username', 'avatar_url', 'name')
+    .countDistinct({ comment_count: 'comment_id', article_count: 'title' })
+    .leftJoin('articles', 'articles.author', 'username')
+    .leftJoin('comments', 'comments.author', 'username')
+    .groupBy('username')
     .where('username', username)
     .then(userArr => {
       if (userArr.length === 0) return Promise.reject({ status: 404, msg: `Could not find a user with the username '${username}'` });
